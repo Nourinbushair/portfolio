60c377ce7b96e1fae091a1e53781c8a8b06d99ca
nourin bushair <nourinbusahir@gmail.com>
2026-06-08 13:48:58 +0530

    Portfolio updates

--- components/About.jsx
+++ components/About.jsx
@@ -16,27 +16,7 @@ export default function About() {
         About Me
       </motion.h2>
 
-      <motion.div
-  initial={{ opacity: 0 }}
-  whileInView={{ opacity: 1 }}
-  transition={{ duration: 1 }}
-  className="mt-10 bg-slate-900 border border-slate-800 rounded-2xl p-8"
->
-  <div className="flex flex-row items-center gap-6">
-
-    <img
-      src="/profile.jpg"
-      alt="Nourin Bushair"
-      className="w-28 h-28 md:w-48 md:h-48 rounded-full object-cover border-4 border-cyan-400"
-    />
-
-    <p className="text-lg text-slate-300 leading-8">
-      I am a passionate BCA student specializing in Cybersecurity at Jain
-      (Deemed-to-be) University...
-    </p>
-
-  </div>
-</motion.div>
+      
     </section>
   );
 }
