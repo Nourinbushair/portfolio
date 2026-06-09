8ecfbed022fd9a7933ac4dfef0f032a143af7007
nourin bushair <nourinbusahir@gmail.com>
2026-06-09 12:01:12 +0530

    redeply

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,6 +22,7 @@ export default function Navbar() {
           <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
           <li><a href="#projects">Projects</a></li>
+          <li><a href="#certificates">Certifications</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
 
@@ -53,6 +54,12 @@ export default function Navbar() {
             <li>
               <a href="#projects" onClick={() => setMenuOpen(false)}>
                 Projects
+              </a>
+            </li>
+            
+            <li>
+              <a href="#certificates" onClick={() => setMenuOpen(false)}>
+                Certifications
               </a>
             </li>
 
